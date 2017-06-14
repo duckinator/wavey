f7faf61653cd7c12505c15a78e2060d6b3007aff
Ellen Marie Dash <me@duckie.co>
2017-06-14 18:54:37 -0400

    ???

--- utils/helpers.rb
+++ utils/helpers.rb
@@ -2,11 +2,14 @@ require "wavey"
 
 module DevHelpers
   def get_samples(wavey_method)
+    channels = 1
+    sample_rate = 100
+
     frequency = 50 # Hz
     amplitude = 0.5 # 50% volume.
     duration  = 10 # seconds.
 
-    Wavey.new.send(wavey_method, frequency, amplitude, duration)
+    Wavey.new(channels, sample_rate).send(wavey_method, frequency, amplitude, duration)
   end
   module_function :get_samples
   public :get_samples
